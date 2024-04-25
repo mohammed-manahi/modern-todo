f0a707eb08b73d95585d2b713d08a3c9eb7645af
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2024-04-25 20:37:04 +0300

    Utilize to do dto for the to do repository

---
+++ ModernTodoBackend/Controllers/TodoController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ModernTodoBackend.DTO;
+using ModernTodoBackend.Repositories;
+
+namespace ModernTodoBackend.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class TodoController : ControllerBase
+{
+    // Todo: crud operation, documentation, add documentation to swagger 
+    private readonly ILogger<TodoController> _logger;
+    private readonly ITodoRepository _todoRepository;
+
+    public TodoController(ILogger<TodoController> logger, ITodoRepository todoRepository)
+    {
+        // Constructor injection for logger and todo repository
+        _logger = logger;
+        _todoRepository = todoRepository;
+    }
+
+
+    [HttpGet("GetAll")]
+    public async Task<IActionResult> GetAll([FromQuery] int pageIndex = 0, int pageSize = 10,
+        string sortColumn = "Name",
+        string sortOrder = "ASC", string filterQuery = null)
+    {
+        try
+        {
+            var result = await _todoRepository.GetAll(pageIndex, pageSize, sortColumn, sortOrder, filterQuery);
+            if (result == null) return BadRequest();
+            return Ok(result);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception.Message);
+            return BadRequest();
+        }
+    }
+
+    [HttpGet("GetAllCompleted")]
+    public async Task<IActionResult> GetAllCompleted([FromQuery] int pageIndex = 0, int pageSize = 10,
+        string sortColumn = "Name",
+        string sortOrder = "ASC", string filterQuery = null)
+    {
+        try
+        {
+            var result = await _todoRepository.GetAllCompleted(pageIndex, pageSize, sortColumn, sortOrder, filterQuery);
+            if (result == null) return BadRequest();
+            return Ok(result);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception.Message);
+            return BadRequest();
+        }
+    }
+
+    [HttpGet("Get/{id}")]
+    public async Task<IActionResult> Get([FromRoute] int id)
+    {
+        try
+        {
+            var result = await _todoRepository.Get(id);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception.Message);
+            return NotFound();
+        }
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> Post([FromBody] TodoDTO entityDto)
+    {
+        try
+        {
+            if (!ModelState.IsValid) return Problem();
+            await _todoRepository.Add(entityDto);
+            return NoContent();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception.Message);
+            return BadRequest();
+        }
+    }
+
+    [HttpPut("Update")]
+    public async Task<IActionResult> Put([FromBody] TodoDTO entityDto)
+    {
+        try
+        {
+            if (entityDto == null) return BadRequest();
+            if (!ModelState.IsValid) return Problem();
+            await _todoRepository.Update(entityDto);
+            return Ok();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception.Message);
+            return BadRequest();
+        }
+    }
+
+    [HttpDelete("Delete/{id}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        try
+        {
+            if (id == null || id == 0) return NotFound();
+            await _todoRepository.Remove(id);
+            return Ok();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception.Message);
+            return BadRequest();
+        }
+    }
+}
--- ModernTodoBackend/Repositories/ITodoRepository.cs
+++ ModernTodoBackend/Repositories/ITodoRepository.cs
@@ -6,21 +6,21 @@ namespace ModernTodoBackend.Repositories;
 
 public interface ITodoRepository : IDisposable
 {
-    public Task<IEnumerable<Todo?>> GetAll(int pageIndex, int pageSize, string sortColumn, string sortOrder,
+    public Task<IEnumerable<Todo>> GetAll(int pageIndex, int pageSize, string sortColumn, string sortOrder,
         string filterQuery);
 
-    public Task<IEnumerable<Todo?>> GetAllCompleted(int pageIndex, int pageSize, string sortColumn, string sortOrder,
+    public Task<IEnumerable<Todo>> GetAllCompleted(int pageIndex, int pageSize, string sortColumn, string sortOrder,
         string filterQuery);
 
-    public Task<Todo?> Get(int id);
+    public Task<Todo> Get(int id);
 
-    public Task<Todo?> Find(Expression<Func<Todo?, bool>> predicate);
+    public Task<Todo> Find(Expression<Func<Todo, bool>> predicate);
 
-    public Task Add(Todo? entity);
+    public Task Add(TodoDTO entityDto);
 
-    public Task Update(TodoDTO? entityDto);
+    public Task Update(TodoDTO entityDto);
 
-    public Task Remove(int? id);
+    public Task Remove(int id);
 
     public Task<int> SaveAsync();
 }
