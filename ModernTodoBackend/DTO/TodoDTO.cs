b48828232d9fd96a3cbdcc441e62c1e96f16abb5
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2024-04-25 20:36:28 +0300

    Add required attribute to name property of to do dto class

--- ModernTodoBackend/DTO/TodoDTO.cs
+++ ModernTodoBackend/DTO/TodoDTO.cs
@@ -6,7 +6,7 @@ public class TodoDTO
 {
     // DTO binder for mapping the updatable properties of todo model
     [Required] public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    [Required]  public string Name { get; set; } = string.Empty;
     public string? Description { get; set; } = string.Empty;
     public DateTime? DueDate { get; set; }
     public bool IsCompleted { get; set; } 
