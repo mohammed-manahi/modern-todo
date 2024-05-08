c725e6a348ac8ee2233b01771fe4d25b5e2de8ce
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2024-05-08 03:20:43 +0300

    Fix navbar for mobile view

--- modern-todo-frontend/src/ui/Navbar.jsx
+++ modern-todo-frontend/src/ui/Navbar.jsx
@@ -1,19 +1,26 @@
-﻿import {AppShell, Space} from "@mantine/core";
+﻿import {AppShell, Group, Space} from "@mantine/core";
 import {NavLink} from "react-router-dom";
 import classes from "./Layout.module.css";
 import ThemeToggle from "./ThemeToggle.jsx";
+import AccountLogout from "../features/account/AccountLogout.jsx";
+import {useAccountContext} from "../features/account/AccountContext.jsx";
 
-function Navbar(){
-   return (
+function Navbar() {
+    const {isAuthenticated} = useAccountContext();
+    return (
 
-       <AppShell.Navbar py="md" px={4}>
-           <NavLink className={classes.control} to={"/"}>Home</NavLink>
-           <NavLink className={classes.control} to={"/login"}>Login</NavLink>
-           <NavLink className={classes.control} to={"/register"}>Register</NavLink>
-           <Space h={"md"}/>
-           <ThemeToggle/>
-       </AppShell.Navbar>
-   );
+        <AppShell.Navbar py="md" px={4}>
+            <NavLink className={classes.control} to={"/"} underline="never">Home</NavLink>
+            {isAuthenticated ? <AccountLogout/> :
+                <>
+                    <NavLink className={classes.control} to={"/login"}>Login</NavLink>
+                    <NavLink className={classes.control} to={"/register"}>Register</NavLink>
+                </>
+            }
+            <Space w={"md"}/>
+            <ThemeToggle/>
+        </AppShell.Navbar>
+    );
 }
 
 export default Navbar;
