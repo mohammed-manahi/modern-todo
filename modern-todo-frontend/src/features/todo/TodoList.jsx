fafc651a287138808e63896e179a2d2f1ad61892
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2024-05-12 21:27:54 +0300

    Basic searching, filtering for todos

--- modern-todo-frontend/src/features/todo/TodoList.jsx
+++ modern-todo-frontend/src/features/todo/TodoList.jsx
@@ -1,7 +1,7 @@
 ﻿import {baseTodoUrl, useTodoContext} from "./TodoContext.jsx";
 import Spinner from "../../ui/Spinner.jsx";
 import {useQuery} from "@tanstack/react-query";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import NotificationArea from "../../ui/NotificationArea.jsx";
 import TodoItem from "./TodoItem.jsx";
 import {
@@ -14,10 +14,10 @@ import {
     Space,
     Kbd,
     Badge,
-    Autocomplete, Select
+    Autocomplete, Select, Button
 } from "@mantine/core";
 import {IconSearch, IconArrowRight} from '@tabler/icons-react';
-import {Form, useForm} from "react-hook-form";
+import {Controller, Form, useForm} from "react-hook-form";
 import AlertArea from "../../ui/AlertArea.jsx";
 import EmptyContent from "../../ui/EmptyContent.jsx";
 
@@ -28,16 +28,22 @@ let sortOrder = "ASC";
 let filterQuery = "";
 
 function TodoList() {
+    // Define a state to manage sort column
+    const [sortColumnValue, setSortColumnValue] = useState(sortColumn);
+    const [sortOrderValue, setSortOrderValue] = useState(sortOrder);
+    
     // Invoke todo context hook 
     const {dispatch} = useTodoContext();
+    
     // Use react form hook for filtering and searching 
     const {handleSubmit, register, formState, control} = useForm();
+  
     // Use the query hook to define the query key and the function 
     const query = useQuery({
         queryKey: ["todo"],
         queryFn: getTodos,
     });
-
+    // Destructure query object
     const {
         data: todos,
         isSuccess,
@@ -48,7 +54,7 @@ function TodoList() {
     } = query;
 
     async function getTodos() {
-        const getAllTodosUrl = `/getAll?PageIndex=${pageIndex}&PageSize=${pageSize}&SortColumn=${sortColumn}&SortOrder=${sortOrder}&FilterQuery=${filterQuery}`;
+        const getAllTodosUrl = `/getAll?PageIndex=${pageIndex}&PageSize=${pageSize}&SortColumn=${sortColumnValue}&SortOrder=${sortOrderValue}&FilterQuery=${filterQuery}`;
         const accessToken = localStorage.getItem("accessToken");
         const response = await fetch(`${baseTodoUrl}${getAllTodosUrl}`,
             {
@@ -71,14 +77,22 @@ function TodoList() {
         }
     }, [dispatch, error, todos, isFetched, isSuccess, isError, isFetching]);
 
-    async function onTodoFilterOrSearch(data) {
+    async function onTodoSearchOrFilter(data) {
+        console.log(data)
         if (data.filterQuery !== null) {
             filterQuery = data.filterQuery;
             return query.refetch();
         }
-        // console.log(data.control);
+        if (data.sortColumn !== null) {
+            setSortColumnValue(() => data.sortColumn);
+            return query.refetch();
+        }
+        if (data.sortOrder !== null) {
+            sortOrder = data.sortOrder;
+            return query.refetch();
+        }
+        return;
     }
-    
 
     if (isFetching) return <Spinner/>
     if (error) return <NotificationArea title={"Error"} color={"red"} message={error.message}/>
@@ -86,21 +100,44 @@ function TodoList() {
         <Container py="xl">
             <AlertArea title="Tip for search use:" description={<Kbd>Enter</Kbd>}/>
             <Space h={"md"}/>
-            <Form onSubmit={handleSubmit(onTodoFilterOrSearch)} control={control}>
+            <Form onSubmit={handleSubmit(onTodoSearchOrFilter)} control={control}>
                 <TextInput
                     id="filterQuery" name="filterQuery"
                     radius="xl" size="md" placeholder="Search title" rightSectionWidth={42}
                     leftSection={<IconSearch style={{width: rem(18), height: rem(18)}} stroke={1.5}/>}
                     {...register('filterQuery')}
                 />
-                {/*<Select*/}
-                {/*    label="Your favorite library"*/}
-                {/*    placeholder="Pick value"*/}
-                {/*    data={['React', 'Angular', 'Vue', 'Svelte']}*/}
-                {/*    searchable*/}
-                {/*    {...register('dropdown')}*/}
-                {/*/>*/}
-
+                <Controller
+                    name="sortColumn"
+                    control={control}
+                    render={({field}) => (
+                        <Select label="Sort by" placeholder="Pick value" value={field.value}
+                                onChange={(value) => {
+                                    field.onChange(value);
+                                    setSortColumnValue(value);
+                                }}
+                                data={[{label: 'Todo Name', value:'Name'}, {label: 'Todo Description', value:'Description'}, {label: 'Todo Due Date', value:'DueDate'}]}
+                                searchable
+                        />
+                    )}
+                />
+                <Controller
+                    name="sortOrder"
+                    control={control}
+                    render={({field}) => (
+                        <Select label="Sort Order" placeholder="Pick value" value={field.value}
+                                onChange={(value) => {
+                                    field.onChange(value);
+                                    setSortOrderValue(value);
+                                }}
+                                data={[{label: 'Ascending', value: 'ASC'}, {label: 'Descending', value: 'DESC'}]}
+                                searchable
+                        />
+                    )}
+                />
+                <Button fullWidth mt="xl" type={"submit"} disabled={isFetching}>
+                    Apply
+                </Button>
             </Form>
             <Space h={"xl"}/>
             {todos.length === 0 ? <EmptyContent/> :
